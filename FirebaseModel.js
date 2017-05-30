a83f94594f43145feea207c6a5b45144fbfa31b2
Tiago Almeida <tiago.b.almeida@gmail.com>
2017-05-30 22:37:42 +0100

    Initial Model file

---
+++ FirebaseModel.js
@@ -0,0 +1,13 @@
+/**
+ * Firebase model implementation
+ *
+ * @namespace
+ * @name openui5.community.ui.model.firebase
+ * @public
+ */
+sap.ui.define(
+    ['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Context'],
+    function(jQuery, ClientModel, Context) {
+	"use strict";
+    }
+);
