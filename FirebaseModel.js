01cd51a47ffb4ab56d5622fe9ee6e144a845062c
Tiago Almeida <tiago.b.almeida@gmail.com>
2017-06-01 22:26:10 +0100

    Added skeleton of FirebaseModel and a test page. Its erroring

--- FirebaseModel.js
+++ FirebaseModel.js
@@ -8,6 +8,37 @@
 sap.ui.define(
     ['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Context'],
     function(jQuery, ClientModel, Context) {
-	"use strict";
+        "use strict";
+
+        /**
+         * Constructor for a new FirebaseModel.
+         *
+         * @class
+         * Model implementation for Firebase model
+         *
+         * @extends sap.ui.model.ClientModel
+         *
+         * @author Tiago Almeida
+         * @version ${version}
+         *
+         * @param {object} oData either the URL where to load the JSON from or a JS object
+         * @param {boolean} bObserve whether to observe the JSON data for property changes (experimental)
+         * @constructor
+         * @public
+         * @alias openui5.community.ui.model.firebase
+         */
+        var FirebaseModel = ClientModel.extend("openui5.community.ui.model.firebase.FirebaseModel", {
+
+            constructor : function(oData) {
+                ClientModel.apply(this, arguments);
+            },
+
+            metadata : {
+                publicMethods : []
+            }
+
+        });
+
+        return FirebaseModel;
     }
 );
